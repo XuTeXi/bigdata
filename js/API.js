2718385119af2abc280c315cda49fbb1c46f657e
xtx <1690517991@qq.com>
2022-09-11 22:30:56 +0800

    API页面全部完成

--- js/API.js
+++ js/API.js
@@ -19,58 +19,121 @@ let apiTypeData = [
     { name: "最新发布", keyword: "银行卡", isBold: true },
     { name: "API私有化部署", keyword: "身份证实名", isBold: true },
 ];
-    let apiHTML = ""
-    apiTypeData.forEach(function (item) {
-        // apiHTML += ` <a>${item.name}</a>`
-        // item.isBold ? bold : ''
-        apiHTML += ` <a class = " ${item.isBold ? 'bold' : ''}">${item.name}</a> `
+let apiHTML = ""
+apiTypeData.forEach(function (item) {
+    // apiHTML += ` <a>${item.name}</a>`
+    // item.isBold ? bold : ''
+    apiHTML += ` <a class = " ${item.isBold ? 'bold' : ''}" key="${item.keyword}">${item.name}</a> `
+})
+// console.log(apiHTML);
+// let apiParent = document.querySelector(".API_fenlei .fenlei_list");
+let apiParent = getEle(".API_fenlei .fenlei_list");
+apiParent.innerHTML = apiHTML;
+// console.log(apiParent);
+
+let tagBtn = getEleAll(".API_fenlei a")
+// console.log(tagBtn);
+tagBtn.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        getEle('.search strong').innerText = this.innerText;
+        getEle('.search input').placeholder = this.getAttribute('key');
     })
-        console.log(apiHTML);
-    let apiParent = document.querySelector(".API_fenlei .fenlei_list");
-        apiParent.innerHTML = apiHTML;
-        console.log(apiParent);
+})
 
- 
-    let showChoose = document.querySelector(".fenlei_list a");
-    // showChoose.innerHTML = apiTypeData;
+// let showChoose = document.querySelector(".fenlei_list a");
+let showChoose = getEle(".fenlei_list a");
+// showChoose.innerHTML = apiTypeData;
 
-    //API列表区域 模拟服务器返回的数据
+
+
+
+//API列表区域 模拟服务器返回的数据
 let listDataArr = [
-  //第一行
-  { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
-  { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
-  { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
-  { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
-  { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
+    //第一行
+    { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
+    { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
+    { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
+    { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
+    { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
 
-  //第二行
-  { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
-  { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
-  { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
-  { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
-  { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
-  //第三行
-  { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
-  { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
-  { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
-  { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
-  { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
-  //第四行
-  { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
-  { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
-  { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
-  { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
-  { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
+    //第二行
+    { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
+    { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
+    { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
+    { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
+    { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
+    //第三行
+    { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
+    { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
+    { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
+    { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
+    { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
+    //第四行
+    { img: "API_01.jpg", name: "2021出行防疫政策指南", price: "免费", isSpecial: false },
+    { img: "API_02.jpg", name: "身份证实名认证", price: "￥0.2000/次", isSpecial: true },
+    { img: "API_03.jpg", name: "天气预报", price: "免费", isSpecial: false },
+    { img: "API_04.jpg", name: "银行卡四元素校验", price: "￥0.3360/次", isSpecial: true },
+    { img: "API_05.jpg", name: "短信API服务", price: "￥0.0400/次", isSpecial: true },
 
 ];
 /* 获取数组，拿到数据 添加li、img、p、h4标签*/
 /* 四个标签的样式在api.css文件中写好 */
 let api_main = ""
-listDataArr.forEach(function(item){
-    api_main += ` <li class="${item.isSpecial ? 'special' : ''}">  <img src="../assets/images/${item.img}"> <p>${item.name}</p>  <h4>${item.price}</h4></li> `
-    })
+listDataArr.forEach(function (item) {
+    api_main += ` <li class="${item.isSpecial ? 'special' : ''}" key = "${item.keyword}">  <img src="../assets/images/${item.img}"> <p>${item.name}</p>  <h4>${item.price}</h4> <a class="apply">申请数据</a> </li> `
+})
 // console.log(api_main);
 /* 在网页中输出 */
-let api_content = document.querySelector(".main ul"); 
-    api_content.innerHTML = api_main;
-  
+// let api_content = document.querySelector(".main ul"); 
+let api_content = getEle(".main ul");
+api_content.innerHTML = api_main;
+
+
+//蒙层效果
+//点击申请数据按钮，展示蒙层
+let applyBtn = getEleAll('.main li .apply')
+let mask = getEle('#mask');
+let login = getEle('.login');
+//遍历所有的数据神奇按钮，添加监听，改变蒙层状态
+applyBtn.forEach(function(item,index){
+    item.addEventListener('click',function(){
+        mask.style.display = "block";
+    })
+})
+//隐藏蒙层
+mask.addEventListener('click',function(){
+    mask.style.display = "none"
+})
+//阻止事件冒泡
+login.addEventListener('click',function(e){
+    e.stopPropagation();
+})
+
+
+//扫码登录
+// let cs_btn = getEle('.login .cs-btn');
+// let an_btn = getEle('.login .an-btn');
+// cs_btn.addEventListener('click',function(){
+//     cs_btn.style.display = 'block';
+//     an_btn.style.display = 'none';
+//     console.log("登录隐藏，二维码显示");
+// })
+// an_btn.addEventListener('click',function(){
+//     cs_btn.style.display = 'none';
+//     an_btn.style.display = 'block';
+//     console.log("二维码隐藏，隐藏显示");
+// })
+let tabs = getEleAll(".login .lg-btn span");
+console.log(tabs);
+let divs = getEleAll(".login .an-btn,.cs-btn");
+console.log(divs );
+tabs.forEach(function(item,index){
+    item.onclick = function(){
+        tabs.forEach(function(element,i){
+            tabs[i].className = '';
+            divs[i].style.display = "none"; 
+        })
+    this.className = "on";
+    divs[index].style.display = "block";
+    }
+})
---
+++ js/utils.js
@@ -0,0 +1,8 @@
+/* 获取单个dom元素 */
+function getEle(ele){
+    return document.querySelector(ele);
+};
+/* 获取多个dom元素 */
+function getEleAll(ele){
+    return document.querySelectorAll(ele);
+};
